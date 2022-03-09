e261bed4e85dc31d98b511cc2975a37185529a1f
Emily Daykin <emilydaykin31@gmail.com>
2022-03-09 13:42:03 +0100

    test branch commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello world!!!</h1>;
+  return (
+    <>
+      <h1>Hello world!!!</h1>
+      <p>Welome to our project!</p>
+    </>
+  );
 }
 
 export default App;
