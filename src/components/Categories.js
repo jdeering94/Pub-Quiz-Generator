423f0fa8a05f17b6f62b1ff65f30547682e4a20c
Emily Daykin <emilydaykin31@gmail.com>
2022-03-09 17:52:48 +0100

    styling and categories linking

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Categories() {
+  const allCategories = [
+    'artliterature',
+    'language',
+    'sciencenature',
+    'general',
+    'fooddrink',
+    'peopleplaces',
+    'geography',
+    'historyholidays',
+    'entertainment',
+    'toysgames',
+    'music',
+    'mathematics',
+    'religionmythology',
+    'sportsleisure'
+  ];
+
+  function cleanUpName(name) {
+    // capitalised in css
+    // Two worded categories to be spaced with a '&'
+    return name;
+  }
+
   return (
     <section>
       <h1>All Categories</h1>
+      <div className='categories-container'>
+        {allCategories.map((category) => (
+          <Link className='category-link' to={`:${category}`} key={category}>
+            <div className='category'>{cleanUpName(category)}</div>
+          </Link>
+        ))}
+      </div>
     </section>
   );
 }
