e780ebaffa8d85c250a61e481635465ac6eb484f
Emily Daykin <emilydaykin31@gmail.com>
2022-03-09 13:55:36 +0100

    added api, axios package, and client file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { cocktailAPI } from './lib/api';
 
 function App() {
-  return <h1>Hello world!!!</h1>;
+  const [state, setState] = React.useState('null');
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const resp = await cocktailAPI();
+      console.log('resp', resp);
+      setState(resp);
+    };
+    getData();
+  }, []);
+
+  console.log('state', state);
+
+  return (
+    <>
+      <h1>Hello world!!!</h1>
+      <p>Welome to our project!</p>
+    </>
+  );
 }
 
 export default App;
