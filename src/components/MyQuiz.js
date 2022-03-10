18f5deea0f2e555d0fc99bed0c61790524d74fe6
Joe Deering <JDeering94@gmail.com>
2022-03-10 10:54:18 +0000

    added random quiz button

--- src/components/MyQuiz.js
+++ src/components/MyQuiz.js
@@ -4,18 +4,33 @@ import { randomQuiz } from '../lib/api';
 function MyQuiz() {
   const [randomQuestions, setRandomQuestions] = React.useState(null);
 
-  React.useEffect(() => {
-    const getData = async () => {
-      const resp = await randomQuiz();
-      console.log('resp', resp.data);
-      setRandomQuestions(resp.data);
-    };
+  const getData = async () => {
+    const resp = await randomQuiz();
+    console.log('resp', resp.data);
+    setRandomQuestions(resp.data);
+  };
+
+  function handleButton(e) {
+    e.preventDefault();
     getData();
-  }, []);
+  }
 
   return (
     <section>
       <h1>My Quiz</h1>
+      <div className="randomQuizContainer">
+        <button onClick={handleButton}>Random Quiz</button>
+        {!randomQuestions ? (
+          <p></p>
+        ) : (
+          randomQuestions.map((question) => (
+            <div className="questionContainer" key={question.question}>
+              <span id="q">{question.question}</span>
+              <span id="a">{question.answer}</span>
+            </div>
+          ))
+        )}
+      </div>
     </section>
   );
 }
