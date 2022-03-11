7830868bcecdc4967708c664166aa09d39e373ea
Joe Deering <JDeering94@gmail.com>
2022-03-11 10:04:56 +0000

    changed about wording sligtly

--- src/components/About.js
+++ src/components/About.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 function About() {
   return (
-    <section className='about-body'>
+    <section className="about-body">
       <h1>About Us</h1>
-      <div className='about-text'>
+      <div className="about-text">
         <p>Hello! Welcome to our pub quiz generator!</p>
         <p>
-          It's pretty straightforward, just navigate to your category of preference, and a choice of
-          thirty random questions will appear. Then, simply click on the questions you want to keep,
-          and they will be added to your basket.
+          It's pretty straightforward, just navigate to your category of
+          preference, and a choice of thirty random questions will appear. Then,
+          simply click on the questions you want to keep, and they will be added
+          to your My Quiz page. If you've changed your mind you can also easily
+          removes questions from the My Quiz page by clicking on them.
         </p>
         <p>
-          Don't have the time to pick each question? No problem! There is a random quiz option that
-          generates 25 questions from random categories. Easy!
+          Don't have the time to pick each question? No problem! There is a
+          random quiz option that generates 25 questions from random categories.
+          Easy!
         </p>
       </div>
     </section>
