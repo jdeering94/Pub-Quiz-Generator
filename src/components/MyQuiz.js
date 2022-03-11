8234f90a7a4508e4739ab2124f9d8eca0278f513
Joe Deering <JDeering94@gmail.com>
2022-03-11 09:28:42 +0000

    added function to remove questions in the myQuiz page

--- src/components/MyQuiz.js
+++ src/components/MyQuiz.js
@@ -11,6 +11,19 @@ function MyQuiz({ allCategories }) {
     }
   }
 
+  React.useEffect(() => {
+    localStorage.setItem('savedQuestions', JSON.stringify(myQuestions));
+  }, [myQuestions]);
+
+  function removeQuestion(ques, ans, qId) {
+    const checkRemoveQuestion = myQuestions.find((ques) => ques.qId === qId);
+    console.log('checkRemoveQuestion', checkRemoveQuestion);
+    if (checkRemoveQuestion) {
+      const filteredQuiz = myQuestions.filter((ques) => ques.qId !== qId);
+      setMyQuestions(filteredQuiz);
+    }
+  }
+
   function handleButton() {
     localStorage.removeItem('savedQuestions');
     setMyQuestions(getState);
@@ -18,24 +31,28 @@ function MyQuiz({ allCategories }) {
 
   console.log('myQuestions', myQuestions);
   return (
-    <section className='myQuiz-section'>
-      <div className='myQuiz-container'>
+    <section className="myQuiz-section">
+      <div className="myQuiz-container">
         <h1>My Quiz</h1>
-        <div className='all-questions-container'>
-          <button className='reset-quiz' onClick={handleButton}>
+        <div className="all-questions-container">
+          <button className="reset-quiz" onClick={handleButton}>
             Reset Quiz
           </button>
           {myQuestions.length === 0 ? (
             <h2>
-              Select questions from the categories by clicking on them, and your list of questions
-              will be displayed here!!
+              Select questions from the categories by clicking on them, and your
+              list of questions will be displayed here!!
             </h2>
           ) : (
-            myQuestions.map(({ question, answer, category }) => (
-              <div className='generated-questionContainer' key={question}>
-                <span id='generated-category'>{allCategories[category]}</span>
-                <span id='generated-question'>{question}</span>
-                <span id='generated-answer'>{answer}</span>
+            myQuestions.map(({ question, answer, category, qId }) => (
+              <div
+                className="generated-questionContainer"
+                key={question}
+                onClick={() => removeQuestion(question, answer, qId)}
+              >
+                <span id="generated-category">{allCategories[category]}</span>
+                <span id="generated-question">{question}</span>
+                <span id="generated-answer">{answer}</span>
               </div>
             ))
           )}
