26afff65afd4792ef56e352fbf1659ea30330965
Emily Daykin <emilydaykin31@gmail.com>
2022-03-14 13:19:18 +0100

    Set max width to all pages for better display on super wide screens

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 function Categories({ allCategories }) {
   return (
     <section className='categories-section'>
-      <h1>All Categories</h1>
-      <div className='categories-container'>
-        {Object.keys(allCategories).map((category) => (
-          <Link className='category-link' to={category} key={category}>
-            <div className='category'>{allCategories[category]}</div>
-          </Link>
-        ))}
+      <div className='categories-wrapper'>
+        <h1>All Categories</h1>
+        <div className='categories-container'>
+          {Object.keys(allCategories).map((category) => (
+            <Link className='category-link' to={category} key={category}>
+              <div className='category'>{allCategories[category]}</div>
+            </Link>
+          ))}
+        </div>
       </div>
     </section>
   );
