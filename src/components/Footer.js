bc0e34024b9c426ae92c420ab04381f5d3edba1f
Emily Daykin <emilydaykin31@gmail.com>
2022-03-17 12:00:20 +0100

    Updated footer with links, and styled bodies of all sections to display footer, and improved design for (small) mobiles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function Footer() {
-  return <footer>Created by Emily Daykin and Joe Deering for GA SEI 62</footer>;
+  return (
+    <footer className='footer'>
+      Created by&nbsp;<a href='https://www.linkedin.com/in/emily-daykin/'>Emily Daykin</a>
+      &nbsp;and&nbsp;<a href='https://www.linkedin.com/in/joe-deering-a009051b0/'>Joe Deering</a>
+      &nbsp;at GA SEI 62
+    </footer>
+  );
 }
 
 export default Footer;
